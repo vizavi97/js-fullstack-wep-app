ef4def40ffb4a39b95fb39c99ddd34382d85ef71
Vizavi <vizavi@yahoo.com>
2020-09-14 13:08:12 +0500

    Initial commit

---
+++ server/src/index.ts
@@ -0,0 +1,68 @@
+import 'reflect-metadata'
+import {MikroORM} from '@mikro-orm/core'
+// import {Post} from "./entities/Post";
+import microConfig from './mikro-orm.config'
+import express from 'express'
+import CONFIG from './config/config'
+import {ApolloServer} from "apollo-server-express";
+import {buildSchema} from "type-graphql";
+import {PostResolver} from "./resolvers/post";
+import {UserResolver} from "./resolvers/user";
+import redis from 'redis'
+import session from 'express-session'
+import connectRedis from 'connect-redis'
+import {__prod__, COOKIE_NAME} from "./constants";
+import {MyContext} from "./types";
+import cors from 'cors'
+
+
+const main = async () => {
+  const orm = await MikroORM.init(microConfig);
+  await orm.getMigrator().up();
+
+  const app = express()
+
+  const RedisStore = connectRedis(session)
+  const redisClient = redis.createClient()
+
+  app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true
+  }))
+  app.use(
+    session({
+      name: COOKIE_NAME,
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, //ten years
+        httpOnly: true,
+        sameSite: 'lax', //csrf
+        secure: __prod__, //cookies only works in https
+      },
+      saveUninitialized: false,
+      secret: 'qwewqewqeqwzdasadxczlml',
+      resave: false,
+    })
+  )
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [PostResolver, UserResolver],
+      validate: false
+    }),
+    context: ({req,res}):MyContext => <MyContext>({em: orm.em, req, res})
+  })
+
+  apolloServer.applyMiddleware({
+    app,
+    cors: false
+  })
+
+  app.listen(CONFIG.PORT, () => console.log('Server is working on port: ', CONFIG.PORT))
+}
+
+
+main().catch(err => console.error(err))
---
+++ server/src/resolvers/user.ts
@@ -0,0 +1,147 @@
+import {Arg, Ctx, Field, InputType, Mutation, ObjectType, Query, Resolver} from "type-graphql";
+import {MyContext} from "../types";
+import {User} from "../entities/User";
+import argon2 from 'argon2'
+import {EntityManager} from '@mikro-orm/postgresql'
+import {COOKIE_NAME} from "../constants";
+
+@InputType()
+class UsernamePasswordInput {
+  @Field()
+  username: string
+  @Field()
+  password: string
+}
+
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], {nullable: true})
+  errors?: FieldError[]
+
+  @Field(() => User, {nullable: true})
+  user?: User
+
+}
+
+
+@Resolver()
+export class UserResolver {
+
+  @Query(() => User, {nullable: true})
+  async me(@Ctx() {req, em}: MyContext) {
+    console.log("Session:", req.session);
+    //you are not logged in
+
+    if (!req.session.userId) {
+      return null
+    }
+    return await em.findOne(User, {id: req.session.userId});
+  }
+
+  @Mutation(() => UserResponse)
+  async register(
+    @Arg('options') options: UsernamePasswordInput,
+    @Ctx() {em, req}: MyContext
+  ): Promise<UserResponse> {
+    if (options.username.length <= 2) {
+      return {
+        errors: [{
+          field: "username",
+          message: "length must be greater than 2"
+        }]
+      }
+    }
+    if (options.password.length <= 3) {
+      return {
+        errors: [{
+          field: "password",
+          message: "length must be greater than 3"
+        }]
+      }
+    }
+    const hashedPassword = await argon2.hash(options.password)
+    let user;
+    try {
+      const result = await (em as EntityManager).createQueryBuilder(User).getKnexQuery().insert({
+        username: options.username,
+        password: hashedPassword,
+        created_at: new Date(),
+        updated_at: new Date()
+      }).returning('*')
+      user = result[0]
+      await em.persistAndFlush(user);
+    } catch (error) {
+      //duplicate username error
+      if (error.code === "23505") {
+        return {
+          errors: [
+            {
+              field: "username",
+              message: "that username already exist"
+            }
+          ]
+        }
+      }
+    }
+    //store user id session
+    //this will set a cookie  on the user
+    //keep them logged in
+    req.session.userId = user.id
+    return {user}
+  }
+
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg('options') options: UsernamePasswordInput,
+    @Ctx() {em, req}: MyContext
+  ): Promise<UserResponse> {
+    const user = await em.findOne(User, {username: options.username})
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "that username doesn't exist",
+          },
+        ],
+      }
+    }
+    const valid = await argon2.verify(user.password, options.password)
+    if (!valid) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "incorrect password",
+          },
+        ],
+      }
+    }
+    console.log("i'm a user: ", user);
+    req.session.userId = user.id
+
+    return {user}
+  }
+  
+  @Mutation(() => Boolean)
+  async logout(@Ctx() {req,res}: MyContext){
+     return await new Promise(resolve => req.session.destroy(err => {
+       res.clearCookie(COOKIE_NAME)
+       if (err) {
+        console.log(err);
+        resolve(false)
+        return;
+      }
+      resolve(true)
+    }))
+  }
+
+}
